fdc64b04e2af1bbfec96cb43a5a841b46ba07728
zxbin <1114706861@qq.com>
2016-08-22 19:53:31 +0800

    1

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-phpinfo();
+// Error Reporting
+error_reporting(E_ALL);
+
+// Check Version
+if (version_compare(phpversion(), '5.3.0', '<') == true) {
+	exit('PHP5.3+ Required');
+}
+
+//version
+define('VERSION','1.0.0');
+$system_path  = 'system';
+if (($_temp = realpath($system_path)) !== FALSE)
+{
+	$system_path = $_temp.DIRECTORY_SEPARATOR;
+}
+else
+{
+	// Ensure there's a trailing slash
+	$system_path = strtr(
+		rtrim($system_path, '/\\'),
+		'/\\',
+		DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+	).DIRECTORY_SEPARATOR;
+}
+define('BASEPATH', $system_path);
+
+require_once(BASEPATH . 'startup.php');
